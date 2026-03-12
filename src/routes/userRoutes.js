cae93c82d6188978944e436c031dda8f18ecf929
UdayPratapSinghTomar <udaytomar2512@gmail.com>
2026-03-12 17:46:15 +0530

    udpate org users api

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/user/userController');
-const { authenticationJWT } = require('../middlewares/authMiddleware');
+const auth = require('../middlewares/authMiddleware');
 const upload = require('../utils/multer');
 
-router.post('/fetch-organization-users', controller.usersByOrgId);
+router.post('/fetch-organization-users', auth, controller.usersByOrgId);
 router.put('/update-profile', upload.single('file'), controller.updateProfile);
 module.exports = router;
