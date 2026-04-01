db9cf2d2e7a0529ba61e462eed9e96e047d13cf1
UdayPratapSinghTomar <udaytomar2512@gmail.com>
2026-04-01 13:56:53 +0530

    logs email api

--- src/utils/sendEmail.js
+++ src/utils/sendEmail.js
@@ -1,39 +1,34 @@
-require('dotenv').config();
 const nodeMailer = require('nodemailer');
 
 const sendEmail = async ({ to, subject, text }) => {
+    console.log("sendEmail function called");
+
     const transporter = nodeMailer.createTransport({
         host: process.env.EMAIL_HOST,
         port: process.env.EMAIL_PORT,
-        // secure: process.env.SECURE,
+        secure: false,
         auth: {
             user: process.env.EMAIL_USER,
             pass: process.env.EMAIL_PASSWORD,
         },
     });
-    // await transporter.sendMail({
-    //     from:   `Bosplan.ai ${process.env.EMAIL_FROM}`,
-    //     to,
-    //     subject,
-    //     text
-    // });
 
     const mailOptions = {
         from: `Bosplan.ai ${process.env.EMAIL_FROM}`,
         to,
         subject,
         text,
-        // html: '<b>Hello!</b> this is an HTML body.' // Optional
     };
 
-    console.log('mailoptions----',mailOptions);
-    transporter.sendMail(mailOptions, (error, info) => {
-        if (error) {
-            return console.log('Error:', error);
-        }
-        console.log('Email sent: ' + info.response);
-    });
-    console.log('transporter----',transporter)
-}; 
+    console.log('mailoptions----', mailOptions);
 
-module.exports = sendEmail;
+    try {
+        await transporter.verify();
+        console.log("SMTP ready");
+
+        const info = await transporter.sendMail(mailOptions);
+        console.log("Email sent:", info.response);
+    } catch (error) {
+        console.log("Error:", error);
+    }
+};
